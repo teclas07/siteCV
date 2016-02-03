6502f5e746003ba67ac7b4b4c7ee5cbcb05c060d
lilrom <lilrom13@gmail.com>
2016-02-03 02:33:06 +0100

    Comment table edit
    adding article_id

--- tables/comment.sql
+++ tables/comment.sql
@@ -28,10 +28,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `comment` (
   `comment_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `content` varchar(255) NOT NULL,
   `post_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `edit_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `edit_timestamp` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
